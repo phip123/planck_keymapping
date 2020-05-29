e2c60ead99cab0ef83da743a1a8ffed52df1c0b3
Philipp <poepsi39@gmail.com>
2020-05-29 10:42:06 +0200

    define name for my layer

--- keymap.c
+++ keymap.c
@@ -19,7 +19,7 @@
 
 enum planck_layers
 {
-    _QWERTY, _COLEMAK, _DVORAK, _LOWER, _RAISE, _PLOVER, _ADJUST
+    _QWERTY, _COLEMAK, _DVORAK, _LOWER, _RAISE, _PLOVER, _ADJUST, _MY_LAYER
 };
 
 enum planck_keycodes
@@ -42,7 +42,7 @@ enum planck_keycodes
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-
+#define MY_LAYER MO(_MY_LAYER)
 const uint16_t PROGMEM
 
 keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,9 +52,7 @@ LAYOUT_ortho_4x12(
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
     KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-    KC_LCTL, KC_LGUI, KC_LALT, MO(
-
-7),
+    KC_LCTL, KC_LGUI, KC_LALT, MY_LAYER,
 
 LOWER, KC_SPC, KC_SPC, RAISE, KC_RALT, KC_RGUI, KC_RSFT, KC_RCTL
 
